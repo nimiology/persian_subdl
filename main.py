8bcbe243e6e32cb63f4b9e5a05802b0d9f36c2c5
nima <nima24jokar@gmail.com>
2021-02-09 11:31:58 +0330

    bug1 fixed

--- main.py
+++ main.py
@@ -4,11 +4,22 @@ from bs4 import BeautifulSoup
 
 FILM = input("write directory \n")
 REQ = requests.get("https://worldsubtitle.info/?s={0}".format(FILM)).text
-LINK = BeautifulSoup(REQ, 'html.parser').find_all(title = FILM)
-ASLI = LINK[0]
+try:
+    LINK = BeautifulSoup(REQ, 'html.parser').find_all(title = FILM)
+    ASLI = LINK[0]
+
+except:
+    print('sorry i cant find it plz choose number of these')
+    LINK = BeautifulSoup(REQ,'html.parser').find_all(class_="cat-post-titel")
+    for i in range(0,len(LINK)):
+        print(f"{i+1} : {LINK[i].string}\n")
+    CHoosed = int(input("\nchoose number! \n"))
+    ASLI=LINK[CHoosed-1]
+    ASLI = BeautifulSoup(f'{ASLI}', 'html.parser').find_all('a')[0]
+
+
 LINKPAGE = ASLI.get('href')
 print(LINKPAGE)
-
 REQ2 = requests.get(LINKPAGE).text
 DOWNLOADLIST = BeautifulSoup(REQ2, 'html.parser').find_all(class_="new-link-3")
 
