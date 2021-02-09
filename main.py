cec4ce326b1fd328efe10d6cec140cea399ca602
nima <nima24jokar@gmail.com>
2021-02-09 10:43:42 +0330

    persian sub added and it get links from worldsubtitle.info correctly

--- main.py
+++ main.py
@@ -6,7 +6,6 @@ FILM = input("write directory \n")
 REQ = requests.get("https://worldsubtitle.info/?s={0}".format(FILM)).text
 LINK = BeautifulSoup(REQ, 'html.parser').find_all(title = FILM)
 ASLI = LINK[0]
-print(ASLI)
 LINKPAGE = ASLI.get('href')
 print(LINKPAGE)
 
@@ -16,7 +15,6 @@ DOWNLOADLIST = BeautifulSoup(REQ2, 'html.parser').find_all(class_="new-link-3")
 LINKS =[]
 for DOWNLOAD in DOWNLOADLIST:
     bs=BeautifulSoup(f'{DOWNLOAD}', 'html.parser').find_all('a')
-    print(bs)
     LINKS.append(bs[0].get("href"))
 
 print(LINKS)
