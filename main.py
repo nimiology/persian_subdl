0ca483e4b2248c1611bc79eb761b3498c5ac8616
nima <nima24jokar@gmail.com>
2021-02-09 10:43:12 +0330

    persian sub added and it get links from worldsubtitle.info correctly

--- main.py
+++ main.py
@@ -2,8 +2,22 @@ import requests
 from bs4 import BeautifulSoup
 
 
-chooser = input("film: 1 \nseries: 2 \nwhich number? \n")
-film = input("write directory \n")
-req = requests.get("https://esubtitle.com/?s={0}/".format(film)).text
-finder = BeautifulSoup(req, 'html.parser').find(text='دانلود زیرنویس فارسی سریال{0}'.format(film))
-print(finder)
+FILM = input("write directory \n")
+REQ = requests.get("https://worldsubtitle.info/?s={0}".format(FILM)).text
+LINK = BeautifulSoup(REQ, 'html.parser').find_all(title = FILM)
+ASLI = LINK[0]
+print(ASLI)
+LINKPAGE = ASLI.get('href')
+print(LINKPAGE)
+
+REQ2 = requests.get(LINKPAGE).text
+DOWNLOADLIST = BeautifulSoup(REQ2, 'html.parser').find_all(class_="new-link-3")
+
+LINKS =[]
+for DOWNLOAD in DOWNLOADLIST:
+    bs=BeautifulSoup(f'{DOWNLOAD}', 'html.parser').find_all('a')
+    print(bs)
+    LINKS.append(bs[0].get("href"))
+
+print(LINKS)
+
