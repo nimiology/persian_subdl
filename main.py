bf1bff8c2ea62e569f8c271e4a6d4b75150f238e
nima <nima24jokar@gmail.com>
2021-02-11 21:45:22 +0330

    preparing

--- main.py
+++ main.py
@@ -2,6 +2,15 @@ import requests
 from bs4 import BeautifulSoup
 from zipfile import ZipFile
 import os
+from persian import PERSSIAN
+
+lan = "Choose your language: \n per : persian\n"
+LANGUAGE = input(lan)
+FILM = input("write film or series name \n")
+
+if LANGUAGE == "per":
+    LINKSss = PERSSIAN(FILM)
+
 
 def download_url(url, save_path, chunk_size=128):
     r = requests.get(url, stream=True)
@@ -9,36 +18,7 @@ def download_url(url, save_path, chunk_size=128):
         for chunk in r.iter_content(chunk_size=chunk_size):
             fd.write(chunk)
 
-FILM = input("write film or series name \n")
-REQ = requests.get("https://worldsubtitle.info/?s={0}".format(FILM)).text
-try:
-    LINK = BeautifulSoup(REQ, 'html.parser').find_all(title = FILM)
-    ASLI = LINK[0]
 
-except:
-    print('\nsorry i cant find it plz choose number of these')
-    LINK = BeautifulSoup(REQ,'html.parser').find_all(class_="cat-post-titel")
-    for i in range(0,len(LINK)):
-        print(f"{i+1} : {LINK[i].string}\n")
-    CHoosed = int(input("\nchoose number! \n"))
-    FILM = LINK[CHoosed-1].string
-    print(FILM)
-    ASLI=LINK[CHoosed-1]
-    ASLI = BeautifulSoup(f'{ASLI}', 'html.parser').find_all('a')[0]
-
-
-LINKPAGE = ASLI.get('href')
-print(LINKPAGE)
-REQ2 = requests.get(LINKPAGE).text
-DOWNLOADLIST = BeautifulSoup(REQ2, 'html.parser').find_all(class_="new-link-3")
-
-LINKSss =[]
-for DOWNLOAD in DOWNLOADLIST:
-    try:
-        bs=BeautifulSoup(f'{DOWNLOAD}', 'html.parser').find_all('a')
-        LINKSss.append(bs[0].get("href"))
-    except:
-        pass
 
 for LINKk in LINKSss:
     try:
---
+++ persian.py
@@ -0,0 +1,33 @@
+import requests
+from bs4 import BeautifulSoup
+
+
+def PERSSIAN(FILM):
+    REQ = requests.get("https://worldsubtitle.info/?s={0}".format(FILM)).text
+    try:
+        LINK = BeautifulSoup(REQ, 'html.parser').find_all(title=FILM)
+        ASLI = LINK[0]
+
+    except:
+        print('\nsorry i cant find it plz choose number of these')
+        LINK = BeautifulSoup(REQ, 'html.parser').find_all(class_="cat-post-titel")
+        for i in range(0, len(LINK)):
+            print(f"{i + 1} : {LINK[i].string}\n")
+        CHoosed = int(input("\nchoose number! \n"))
+        FILM = LINK[CHoosed - 1].string
+        print(FILM)
+        ASLI = LINK[CHoosed - 1]
+        ASLI = BeautifulSoup(f'{ASLI}', 'html.parser').find_all('a')[0]
+
+    LINKPAGE = ASLI.get('href')
+    print(LINKPAGE)
+    REQ2 = requests.get(LINKPAGE).text
+    DOWNLOADLIST = BeautifulSoup(REQ2, 'html.parser').find_all(class_="new-link-3")
+    LINKSss = []
+    for DOWNLOAD in DOWNLOADLIST:
+        try:
+            bs = BeautifulSoup(f'{DOWNLOAD}', 'html.parser').find_all('a')
+            LINKSss.append(bs[0].get("href"))
+        except:
+            pass
+    return LINKSss
